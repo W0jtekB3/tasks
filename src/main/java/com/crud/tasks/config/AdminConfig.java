2a4d0a496da0685ef029a346825f5289d94fc376
Wojciech Bednarski <wojciech.bednarski.wb@gmail.com>
2024-07-31 20:57:02 +0200

    24.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -1,12 +1,43 @@
 package com.crud.tasks.config;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
 public class AdminConfig {
-    @Value("${admin.mail}")
-    private String adminMail;
+
+    @Value("${info.app.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getCompanyGoal() {
+        return companyGoal;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
 }
